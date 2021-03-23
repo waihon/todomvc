5985104a89504b24c49064c573acc349c2628e9d
Waihon Yew <yewwaihon@gmail.com>
2021-03-23 22:00:33 +0800

    Import shared CSS so they become part of our build

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,6 +5,8 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function (defaults) {
   let app = new EmberApp(defaults, {
     // Add options here
+    app.import('node_modules/todomvc-common/base.css');
+    app.import('node_modules/todomvc-app-css/index.css');
   });
 
   // Use `app.import` to add additional libraries to the generated
