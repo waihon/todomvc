e55b1478bef2db40a32abff0ed79062c842bd44e
Waihon Yew <yewwaihon@gmail.com>
2021-03-23 22:10:38 +0800

    Move app.import to after the declaration of app

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,12 +5,12 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function (defaults) {
   let app = new EmberApp(defaults, {
     // Add options here
-    app.import('node_modules/todomvc-common/base.css');
-    app.import('node_modules/todomvc-app-css/index.css');
   });
 
   // Use `app.import` to add additional libraries to the generated
   // output files.
+  app.import('node_modules/todomvc-common/base.css');
+  app.import('node_modules/todomvc-app-css/index.css');
   //
   // If you need to use different assets in different
   // environments, specify an object as the first parameter. That
