a0673656b4d76de149d4d8f96560814f688e5125
Waihon Yew <yewwaihon@gmail.com>
2021-03-25 06:47:07 +0800

    Create Todo class to track text and isCompleted

--- app/services/todo-data.js
+++ app/services/todo-data.js
@@ -1,4 +1,15 @@
 import Service from '@ember/service';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+
+class Todo {
+  @tracked text = '';
+  @tracked isCompleted = false;
+
+  constructor(text) {
+    this.text = text;
+  }
+}
 
 export default class TodoDataService extends Service {
 }
