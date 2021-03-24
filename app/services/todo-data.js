c3af1d1bc9fea6548fefe3cbea9193e2b28437e6
Waihon Yew <yewwaihon@gmail.com>
2021-03-25 06:48:45 +0800

    Update TodoDataService to track todos and allow add

--- app/services/todo-data.js
+++ app/services/todo-data.js
@@ -12,4 +12,12 @@ class Todo {
 }
 
 export default class TodoDataService extends Service {
+  @tracked todos = [];
+
+  @action
+  add(text) {
+    let newTodo = new Todo(text);
+
+    this.todos = [...this.todos, newTodo];
+  }
 }
