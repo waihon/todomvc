4b20b5f790312b40d93f8ffc29367eb65758abf3
Waihon Yew <yewwaihon@gmail.com>
2021-03-27 05:20:01 +0800

    Expose the todo-data service via the todos property

--- app/components/todo-list.js
+++ app/components/todo-list.js
@@ -1,4 +1,6 @@
 import Component from '@glimmer/component';
+import { inject as service } frorm '@ember/service';
 
 export default class TodoListComponent extends Component {
+  @service('todo-data') todos;
 }
