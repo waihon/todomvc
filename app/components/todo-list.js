02bf28fe36c132ee1f353551bb2ba9aa38a269dd
Waihon Yew <yewwaihon@gmail.com>
2021-04-02 01:07:52 +0800

    Replace todos service in todo-list with todoData

--- app/components/todo-list.js
+++ app/components/todo-list.js
@@ -2,5 +2,5 @@ import Component from '@glimmer/component';
 import { inject as service } from '@ember/service';
 
 export default class TodoListComponent extends Component {
-  @service('todo-data') todos;
+  @service todoData;
 }
