2d858b65961bd96aa66345e1e1233186de4d6752
Waihon Yew <yewwaihon@gmail.com>
2021-04-02 17:21:10 +0800

    Import additional classes and functions into todo

--- app/components/todo.js
+++ app/components/todo.js
@@ -1,5 +1,10 @@
 import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
+import { scheduleOnce } from '@ember/runloop';
+
+import { isEnterKey, isEscapeKey } from 'todomvc/utils/key';
 
 export default class TodoComponent extends Component {
   @service todoData;
