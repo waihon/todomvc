9488b8ec807ebc5792ab0ce85b2d87ad070f1db8
Waihon Yew <yewwaihon@gmail.com>
2021-04-03 00:20:10 +0800

    Allow toggle all todos

--- app/components/todo-list.js
+++ app/components/todo-list.js
@@ -1,6 +1,35 @@
 import Component from '@glimmer/component';
+import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
+import { tracked } from '@glimmer/tracking';
 
 export default class TodoListComponent extends Component {
   @service todoData;
+
+  @tracked canToggle = true;
+
+  get areAllCompleted() {
+    let { todos } = this.args;
+
+    return todos.every(todo => {
+      return todo.completed;
+    });
+  }
+
+  @action toggleAll() {
+    let { todos } = this.args;
+    let isCompleted = this.areAllCompleted;
+
+    todos.forEach(todo => todo.completed = !isCompleted);
+
+    this.todoData.persist();
+  }
+
+  @action disableToggle() {
+    this.canToggle = false;
+  }
+
+  @action enableToggle() {
+    this.canToggle = true;
+  }
 }
