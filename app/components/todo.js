8ff255f14dbae981d5cbdafc648ef8af7c951a10
Waihon Yew <yewwaihon@gmail.com>
2021-04-02 23:30:29 +0800

    Allow individual todo to be edited

--- app/components/todo.js
+++ app/components/todo.js
@@ -8,4 +8,55 @@ import { isEnterKey, isEscapeKey } from 'todomvc/utils/key';
 
 export default class TodoComponent extends Component {
   @service todoData;
+
+  @tracked isEditing = false;
+
+  @action edit() {
+    this.originalTitle = this.args.todo.title;
+
+    this.isEditing = true;
+    this.args.onStartEditing();
+    scheduleOnce('afterRender', this, 'focus');
+  }
+
+  @action complete() {
+    let { todo } = this.args;
+
+    this.todoData.toggle(todo);
+  }
+
+  @action finish() {
+    if (!this.isEditing) return;
+
+    let { todo } = this.args;
+    let pendingTitle = this.inputElement.value;
+
+    if (!pendingTitle) {
+      this.todoData.remove(todo);
+      return;
+    }
+
+    this.todoData.updateTitle(todo, pendingTitle);
+
+    this.isEditing = false;
+    this.args.onFinishEditing();
+  }
+
+  @action onKeyDown(event) {
+    if (isEnterKey(event)) {
+      event.target.blur();
+    } else if (isEscapeKey(event)) {
+      this.isEditing = false;
+    }
+  }
+
+  @action createRef(inputElement) {
+    this.inputElement = inputElement;
+  }
+
+  focus() {
+    if (!this.inputElement) return;
+
+    this.inputElement.focus();
+  }
 }
